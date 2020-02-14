fe724c27abbb2309e014a76279c64f07fdc7d680
bl2b <bl2b@live.com>
2020-02-15 00:01:13 +0800

    change port

---
+++ src/environments/environment prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCFkutS1lzUm5PWLiWCQ6IDreneUI90LSU',
+    authDomain: 'sample-crud-10d2b.firebaseapp.com',
+    databaseURL: 'https://sample-crud-10d2b.firebaseio.com',
+    projectId: 'sample-crud-10d2b',
+    storageBucket: 'sample-crud-10d2b.appspot.com',
+    messagingSenderId: '1027836780532',
+    appId: '1:1027836780532:web:636b139262aaeb930463a1',
+  }
+};
