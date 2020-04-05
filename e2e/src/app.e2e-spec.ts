4505b950e6ec77ed09e54cbc10086a424a626257
bl2b <bl2b@live.com>
2020-04-05 13:31:59 +0800

    change dist

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-material7-firebase-crud-app!');
+    expect(page.getTitleText()).toEqual('Welcome to angular-firebase!');
   });
 
   afterEach(async () => {
